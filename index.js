3ca84188a8ccac9a6e651705b869409593b40c47
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-08-16 10:11:54 -0700

    created add department function

--- index.js
+++ index.js
@@ -26,12 +26,14 @@ const questions = () => {
             case "View ALL Employees":
                 viewAllEmployees(connection);
                 break;
-            case "View All Employees By Department":
+            case "View All Departments":
+                viewAllDepartments(connection);
                 break;
             case "View ALL Roles":
                 viewAllRoles(connection);
                 break;
             case "Add a Department":
+                addDepartment(connection)
                 break;
             case "Add a Role":
                 break;
@@ -45,7 +47,27 @@ const questions = () => {
     })
 };
 questions();
-  
+
+//View all employees 
+function viewAllEmployees(connection) {
+    connection.query('SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id',
+    function(err, res) {
+        if(err) throw err;
+        console.log("\n Employees Retrieved from DB \n");
+        console.table(res);
+        questions();
+    });
+};
+
+//View all departments
+function viewAllDepartments(connection) {
+    connection.query('SELECT * FROM department', function(err, res) {
+        if(err) throw err;
+        console.log("\n Departments Retrieved from DB \n");
+        console.table(res);
+        questions();
+    });
+};
 
 //View all roles
 function viewAllRoles(connection) {
@@ -57,13 +79,23 @@ function viewAllRoles(connection) {
     });
 };
 
-//View all employees (need to update query)
-function viewAllEmployees(connection) {
-    connection.query('SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id, role.title, department.name AS department, role.salary FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department on role.department_id = department.id',
-    function(err, res) {
-        if(err) throw err;
-        console.log("\n Employees Retrieved from DB \n");
-        console.table(res);
-        questions();
-    });
+//Add a department
+function addDepartment(connection) {
+    inquirer.prompt({
+        type: "input",
+        name: "department",
+        message: "What department would you like to add?"
+    })
+    .then(function(answer) {
+        connection.query('INSERT INTO department SET ?',
+        {
+            name: answer.department
+        },
+        function(err, res) {
+            if(err) throw err;
+            console.log("\n Departments Retrieved from DB \n");
+            console.table(res);
+            questions();
+        });
+    })
 };
